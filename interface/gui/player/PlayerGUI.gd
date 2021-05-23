4dd85f0c3d5c1f2f1080c6da0b911c6527f603d7
B0gdan101 <bogdancristea15@gmail.com>
2021-05-23 18:59:46 +0300

    Added lifebar for monsters and player

---
+++ interface/gui/player/PlayerGUI.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func initialize(health_node, purse):
+	$LifeBar.initialize(health_node)
+	health_node.connect('health_depleted', self, '_on_Player_Health_health_depleted')
+
+func _on_Player_Health_health_depleted():
+	$AnimationPlayer.play("fade_out")
